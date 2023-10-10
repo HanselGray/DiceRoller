bfacf454bbdd89fd9ae4e60e1f5e74f1ffa09be4
HanselGray <124652238+HanselGray@users.noreply.github.com>
2023-10-10 10:38:04 +0700

    Add graphical dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,23 +1,25 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var result: TextView
-
+    private lateinit var diceFace: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val tossDice: Button = findViewById(R.id.tossDice)
+        result = findViewById(R.id.textView)
+        diceFace = findViewById(R.id.diceImage)
         tossDice.setOnClickListener {
             Toast.makeText(this, "Dice rolled!", Toast.LENGTH_SHORT).show()
-            result = findViewById(R.id.textView)
             rollDice()
         }
     }
@@ -25,10 +27,18 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun rollDice() {
-        val dice = Dice(16)       
+        val dice = Dice()
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        result.text = diceRoll.toString()
+        result.textSize = 34F
+        when (diceRoll){
+            1 -> diceFace.setImageDrawable(getDrawable(R.drawable.dice_1))
+            2 -> diceFace.setImageDrawable(getDrawable(R.drawable.dice_2))
+            3 -> diceFace.setImageDrawable(getDrawable(R.drawable.dice_3))
+            4 -> diceFace.setImageDrawable(getDrawable(R.drawable.dice_4))
+            5 -> diceFace.setImageDrawable(getDrawable(R.drawable.dice_5))
+            6 -> diceFace.setImageDrawable(getDrawable(R.drawable.dice_6))
+        }
     }
 }
 
